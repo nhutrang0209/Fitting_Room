887d110bfbef9ec247bc5764f61bb5367ef22718
TrangNN <trangnn@rocketstudio.com.vn>
2024-05-06 00:47:21 +0700

    Add map, animation

---
+++ Assets/Fitting Room/Scripts/SaveAndLoad/Clothing.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Fitting_Room
+{
+    public enum Category
+    {
+        Dress,
+        Shirt,
+        Pants
+    }
+    
+    [CreateAssetMenu(menuName = "Fitting Room/Clothing", fileName = "Cloth")]
+    public class Clothing : ScriptableObject
+    {
+        [SerializeField] private string clothName;
+        [SerializeField] private Category category;
+        
+    }
+}
